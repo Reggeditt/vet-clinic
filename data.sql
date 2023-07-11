aa57296e99de76fa3d2c3beb64f22c9a0b31db3a
Reggeditt <tkodjosarso@gmail.com>
2023-07-11 07:33:22 +0000

    insert data

--- data.sql
+++ data.sql
@@ -1,5 +1,9 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, weght_kg, neutered, escape_attempts) VALUES ('Agumon', DATE '2020-02-03', 10.23, true, 0);
+
+INSERT INTO animals (name, date_of_birth, weght_kg, neutered, escape_attempts) VALUES ('Gabumon', DATE '2018-11-15', 8.0, true, 2);
+
+INSERT INTO animals (name, date_of_birth, weght_kg, neutered, escape_attempts) VALUES ('Pikachu', DATE '2021-01-07', 15.04, false, 1);
+
+INSERT INTO animals (name, date_of_birth, weght_kg, neutered, escape_attempts) VALUES ('Devimon', DATE '2017-05-12', 11.0, true, 5);
