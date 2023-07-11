dda86fbd4a85fb939d991e1c77f489b28eae2160
Reggeditt <tkodjosarso@gmail.com>
2023-07-11 07:51:34 +0000

    update solution

--- queries.sql
+++ queries.sql
@@ -8,10 +8,10 @@ SELECT * FROM animals WHERE neutered = 't' AND escape_attempts < 3;
 
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 
-SELECT name, escape_attempts FROM animals WHERE weght_kg > 10.5;
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
 SELECT * FROM animals WHERE neutered = 't';
 
 SELECT * FROM animals WHERE name != 'Gabumon';
 
-SELECT * FROM animals WHERE weght_kg >= 10.4 AND weght_kg <= 17.3;
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
