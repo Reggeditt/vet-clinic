411669f84d8deb466708b31d03330b6c00e7238f
Reggeditt <tkodjosarso@gmail.com>
2023-07-11 07:33:40 +0000

    write queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,17 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon';
+
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+SELECT * FROM animals WHERE neutered = 't' AND escape_attempts < 3;
+
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+
+SELECT name, escape_attempts FROM animals WHERE weght_kg > 10.5;
+
+SELECT * FROM animals WHERE neutered = 't';
+
+SELECT * FROM animals WHERE name != 'Gabumon';
+
+SELECT * FROM animals WHERE weght_kg >= 10.4 AND weght_kg <= 17.3;
