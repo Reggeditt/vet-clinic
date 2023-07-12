29d818b3b60306f27eac441212f5099ee8f0e513
Reggeditt <tkodjosarso@gmail.com>
2023-07-12 21:32:14 +0000

    update schema

--- schema.sql
+++ schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE animals (
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
-    weight_kg decimal,
-    species varchar(100)
+    weight_kg decimal
 );
+
+ALTER TABLE animals ADD species varchar(255);
