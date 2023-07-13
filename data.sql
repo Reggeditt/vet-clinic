8ab8ec15c013cb84dfbb57b971cd13e686687014
Tetteh Kodjo-Sarso <Tkodjosarso@gmail.com>
2023-07-13 07:03:37 +0000

    Revert "Update and query branch"

--- data.sql
+++ data.sql
@@ -1,10 +1,5 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
-VALUES ('Charmander', DATE '2020-02-08', -11.00, false, 0),
-('Plantmon', DATE '2021-11-15', -5.7, true, 2),
-('Squirtle', DATE '1993-04-02', -12.13, false, 3),
-('Angemon', DATE '2015-06-12', -45.00, true, 1),
-('Boarmon', DATE '2005-06-07', 20.40, true, 7),
-('Blossom', DATE '1998-10-13', 17.00, true, 3),
-('Ditto', DATE '2022-05-14', 22.00, true, 4);
+INSERT INTO animals (name) VALUES ('Luna');
+INSERT INTO animals (name) VALUES ('Daisy');
+INSERT INTO animals (name) VALUES ('Charlie');
--- schema.sql
+++ schema.sql
@@ -1,12 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id BIGSERIAL PRIMARY KEY,
-    name varchar(100),
-    date_of_birth date,
-    escape_attempts integer,
-    neutered boolean,
-    weight_kg decimal
+    name varchar(100)
 );
-
-ALTER TABLE animals ADD species varchar(255);
