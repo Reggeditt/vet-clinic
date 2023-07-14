913e26556b8a4619698d67fb7ad2feed14fe00c1
Reggeditt <tkodjosarso@gmail.com>
2023-07-14 03:39:21 +0000

    many to many relationship implementation

--- data.sql
+++ data.sql
@@ -19,21 +19,61 @@
 -- ('Ditto', DATE '2022-05-14', 22.00, true, 4);
 
 -- day 3
-INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34),
-('Jennifer Orwell', 19),
-('Bob', 45),
-('Melody Pond', 77),
-('Dean Winchester', 14),
-('Jodie Whittaker', 38);
+-- INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34),
+-- ('Jennifer Orwell', 19),
+-- ('Bob', 45),
+-- ('Melody Pond', 77),
+-- ('Dean Winchester', 14),
+-- ('Jodie Whittaker', 38);
 
-INSERT INTO species (name) VALUES ('pokemon'),
-('digimon');
+-- INSERT INTO species (name) VALUES ('pokemon'),
+-- ('digimon');
 
-UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
-UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
+-- UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+-- UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
 
-UPDATE animals SET owner_id = 1 WHERE name IN ('Agumon');
-UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
-UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
-UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
-UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+-- UPDATE animals SET owner_id = 1 WHERE name IN ('Agumon');
+-- UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+-- UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+-- UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+-- UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+
+-- day 4
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES
+    ('William Tatcher', 45, DATE '2000-04-23'),
+    ('Maisy Smith', 26, DATE '2019-01-17'),
+    ('Stephanie Mendez', 64, DATE '1981-05-04'),
+    ('Jack Harkness', 38, DATE '2008-06-08');
+
+-- Insert data for vet specialties
+INSERT INTO specializations (vet_id, species)
+VALUES
+    (1, 'Pokemon'),
+    (3, 'Digimon'),
+    (3, 'Pokemon'),
+    (4, 'Digimon');
+
+-- Insert data for visits
+INSERT INTO visits (animal_id, vet_id, visit_date)
+VALUES
+    (1, 1, DATE '2020-05-24'),
+    (1, 3, DATE '2020-07-22'),
+    (2, 4, DATE '2021-02-02'),
+    (3, 2, DATE '2020-01-05'),
+    (3, 2, DATE '2020-03-08'),
+    (3, 2, DATE '2020-05-14'),
+    (4, 3, DATE '2021-05-04'),
+    (5, 4, DATE '2021-02-24'),
+    (6, 2, DATE '2019-12-21'),
+    (6, 1, DATE '2020-08-10'),
+    (6, 2, DATE '2021-04-07'),
+    (7, 3, DATE '2019-09-29'),
+    (8, 4, DATE '2020-10-03'),
+    (8, 4, DATE '2020-11-04'),
+    (9, 4, DATE '2019-01-24'),
+    (9, 4, DATE '2019-05-15'),
+    (9, 4, DATE '2020-02-27'),
+    (9, 4, DATE '2020-08-03'),
+    (10, 3, DATE '2020-05-24'),
+    (10, 1, DATE '2021-01-11');
--- schema.sql
+++ schema.sql
@@ -25,3 +25,26 @@ CREATE TABLE species (
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD species_id BIGINT REFERENCES species(id);
 ALTER TABLE animals ADD owner_id BIGINT REFERENCES owners(id);
+
+-- Create the "vets" table
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INTEGER,
+    date_of_graduation DATE
+);
+
+-- Create the "specializations" join table
+CREATE TABLE specializations (
+    vet_id INTEGER REFERENCES vets(id),
+    species VARCHAR(255),
+    PRIMARY KEY (vet_id, species)
+);
+
+-- Create the "visits" join table
+CREATE TABLE visits (
+    animal_id INTEGER REFERENCES animals(id),
+    vet_id INTEGER REFERENCES vets(id),
+    visit_date DATE,
+    PRIMARY KEY (animal_id, vet_id)
+);
